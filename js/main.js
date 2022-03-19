063f5e6bbc2023d424c9ed9b00bb3ee1ae99e285
Harwinder Singh <hrwndr97@gmail.com>
2022-03-20 00:02:35 +0530

    Initial Commit

---
+++ js/main.js
@@ -0,0 +1,21 @@
+const backHomeBtn = document.querySelector('.backhome')
+
+
+let prevScrollpos = window.pageYOffset;
+window.onscroll = function () {
+    const currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+        document.querySelector("nav").style.top = "0";
+        backHomeBtn.style.display = 'none'
+    } else {
+        document.querySelector("nav").style.top = "-100px";
+        backHomeBtn.style.display = 'block'
+    }
+    prevScrollpos = currentScrollPos;
+}
+
+const splide = new Splide('.splide', {
+    autoplay: true
+})
+splide.on('autoplay', e => console.log(e))
+splide.mount()
